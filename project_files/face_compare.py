43ef02828572e77f50abd145e5d65118f9b4ce68
akshat <akshat.science@gmail.com>
2020-03-29 14:38:38 +0530

    changes done

--- project_files/face_compare.py
+++ project_files/face_compare.py
@@ -18,12 +18,25 @@ while True:
 	gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 	face_size=detect.detectMultiScale(gray,1.3,5)
 	for (x,y,h,w) in face_size:
-		faces=frame[y:y+h,x:x+w]
+		faces=gray[y:y+h,x:x+w]
+	
 	save_img='/home/akshat/new_save1/user'+str(c)+'.jpg'
 	#resize=cv2.resize(frame,(200,200))		
 	cv2.imwrite(save_img,faces)
 	if c==5:
 		break
+# face comparison
+my_pic=face_recognition.load_image_file('/home/akshat/new_save1/user2.jpg')
+encode1=face_recognition.face_encodings(my_pic)[0]
+
+cmp_pic=face_recognition.load_image_file('/home/akshat/img_save/user1.jpg')
+encode2=face_recognition.face_encodings(cmp_pic)[0]
+
+result=face_recognition.compare_faces([encode1],encode2)
+if result[0]==True:
+	print('true')
+else:
+	print('false')
 
 shutil.rmtree(r'/home/akshat/new_save1')
 cam.release()
